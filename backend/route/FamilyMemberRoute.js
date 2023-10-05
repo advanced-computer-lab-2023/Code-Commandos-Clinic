73035a0fe9cf0afca8498d22a11cfccbe21d36c7
mohammedshafie1 <mohamed_shafie12@hotmail.co.uk>
2023-10-05 22:29:57 +0300

    familymember route enhanced

--- backend/route/FamilyMemberRoute.js
+++ backend/route/FamilyMemberRoute.js
@@ -3,6 +3,7 @@ const FamilyMember = require("../model/FamilyMember");
 const router = express.Router();
 const asyncHandler = require('express-async-handler');
 
+// task 18
 router.post('/',asyncHandler(async(req,res) => {
     const {name,NationalId,age,gender,relation,patient} = req.body
     
@@ -10,8 +11,10 @@ router.post('/',asyncHandler(async(req,res) => {
         const familymember = await FamilyMember.create({name,NationalId,age,gender,relation,patient})
         res.status(200).json(familymember)
       } catch(error){
-        throw new Error('cannot be added')
+        throw new Error(error.message)
       }
     }))
+
+router.get()
 
 module.exports=router
