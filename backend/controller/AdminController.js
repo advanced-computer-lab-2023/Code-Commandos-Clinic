9cebf276e7f286b08a9f0028a633ff62af9060a3
funny flea <funnyflea7@Yaras-MacBook-Pro.local>
2023-10-07 21:52:31 +0300

    c

--- backend/controller/AdminController.js
+++ backend/controller/AdminController.js
@@ -2,10 +2,11 @@ const DoctorModel = require('../model/Doctor')
 const PatientModel = require('../model/Patient')
 const AdminModel = require('../model/Admin')
 const mongoose = require('mongoose')
+const asyncHandler = require('express-async-handler')
 
 //requirement  7
 // add another adminstrator with a set username and password
-const addAdmin = async(req,res) =>
+const addAdmin = asyncHandler(async(req,res) =>
 {
         const{username,password}= req.body
      
@@ -15,10 +16,12 @@ const addAdmin = async(req,res) =>
          res.status(200).json(admin)
         } catch(error)
         {
-         res.status(400).json({error: "cannot add admin"})
+         res.status(400)//.json({error: "cannot add admin"})
+         //throw new Error('cannot add admin')
+         throw new Error('error.message')
         }
         
-}
+})
 
 module.exports={
     addAdmin
