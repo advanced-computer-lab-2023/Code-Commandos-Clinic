1d3ac6321ec9277eef9a2a3e8ce67ae41289e332
funny flea <funnyflea7@Yaras-MacBook-Pro.local>
2023-10-08 19:52:03 +0300

    req8

--- backend/controller/AdminController.js
+++ backend/controller/AdminController.js
@@ -18,12 +18,73 @@ const addAdmin = asyncHandler(async(req,res) =>
         {
          res.status(400)//.json({error: "cannot add admin"})
          //throw new Error('cannot add admin')
-         throw new Error('error.message')
+         throw new Error(error.message)
         }
         
 })
 
+//requirement 8
+//remove a doctor/patient/ Admin from the system
+
+const removePatient =asyncHandler( async (req,res) =>
+{
+    const { id } =req.params //grapping id from prams
+    
+    const patient =await PatientModel.findOneAndDelete({_id:id})
+
+    if(!patient){
+        return res.status(400)
+        throw new Error(error.message)
+
+    }
+    res.status(200).json(workout)
+
+})
+
+const removeDoctor =asyncHandler( async (req,res) =>
+{
+    const { id } =req.params //grapping id from prams
+    
+    const Doctor =await DoctorModel.findOneAndDelete({_id:id})
+
+    if(!Doctor){
+        return res.status(400)
+        throw new Error(error.message)
+
+    }
+    res.status(200).json(workout)
+
+})
+
+const removeAdmin =asyncHandler( async (req,res) =>
+{
+    const { id } =req.params //grapping id from prams
+    
+    const admin =await AdminModel.findOneAndDelete({_id:id})
+
+    if(!admin){
+        return res.status(400)
+        throw new Error(error.message)
+
+    }
+    res.status(200).json(admin)
+
+})
+
+const getAdmin=async (req,res) =>
+{
+   //const workouts=await Workout.find({resp:20})
+   const workouts=await AdminModel.find({})
+   
+   res.status(200).json(workouts) //worouts??????
+
+}
+
 module.exports={
-    addAdmin
+    addAdmin,
+    removeAdmin,
+    removeDoctor,
+    removePatient,
+    getAdmin
 }
 
--- backend/route/AdminRoute.js
+++ backend/route/AdminRoute.js
@@ -2,9 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 const {
-  addAdmin
+  addAdmin,removePatient,getAdmin,removeAdmin,removeDoctor,
 } = require('../controller/AdminController')
 
+router.get('/',getAdmin)
 router.post('/',addAdmin)
+router.delete('/:id',removeAdmin)
+router.delete('/:id',removeDoctor)
+router.delete('/:id',removePatient)
 module.exports = router
-
