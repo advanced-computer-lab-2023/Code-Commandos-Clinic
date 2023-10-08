d08d13f75171b162e6f170a8d320edb9bac37a07
ziad-shams <ziadamr2002@outlook.com>
2023-10-08 17:18:43 +0300

    refactoring

--- backend/route/DoctorRoute.js
+++ backend/route/DoctorRoute.js
@@ -15,9 +15,8 @@ router.route('/createDoctor').post(createDoctor)
 router.route('/viewDoctor/:id').get(viewDoctor)
 router.route('/filterBySpecialityAndDate/:speciality/:date').get(filterBySpecialityAndDate)
 //ziad: req 14 update doctor's email, hourlyRate, affiliation
-router.route('/updateDoctor').put(updateDoctor)
-router.route('/:id').put(updateDoctor)
 router.route('/getDoctors').get(getDoctors)
 router.route('/updateDoctor').put(updateDoctor)
+router.route('/updateDoctor/:id').put(updateDoctor)
 
 module.exports = router
