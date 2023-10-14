dd6660cc3dea1c8d71571c73bc2ae7095d9fb76e
ebramfadl <ebram@ntsal.com>
2023-10-14 17:06:39 +0300

    Health record details added

--- frontend/src/components/PatientInfo.js
+++ frontend/src/components/PatientInfo.js
@@ -3,17 +3,23 @@ import HealthRecordDetails from './HealthRecordDetails';
 
 const PatientInfo = ({ patient }) => {
     const [healthRecord, setHealthRecord] = useState(null);
-
     useEffect(() => {
-        // Fetch health record when the component mounts
         fetchHealthRecord();
     }, []);
 
     const fetchHealthRecord = async () => {
-        const response = await fetch(`/api/patient/getInfoHealthPatient/${patient._id}`);
-        const healthRecord = await response.text(); // Make sure to await the response text
-        setHealthRecord(healthRecord);
-        console.log(healthRecord)
+        try {
+            const response = await fetch(`/api/patient/getInfoHealthPatient/${patient._id}`);
+            if (response.ok) {
+                const hR = await response.json()
+                setHealthRecord(hR);
+            } else {
+                alert(await response.text())
+            }
+        }
+        catch (error){
+            alert(error)
+        }
     };
 
     return (
