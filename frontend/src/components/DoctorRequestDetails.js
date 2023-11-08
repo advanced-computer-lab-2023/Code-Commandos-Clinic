f24212871fe0d87120343f17bfdd87b1a27b5f7a
OSHashem <oh90002@gmail.com>
2023-11-08 20:32:51 +0200

    accept doc request (req 15) frontend

--- frontend/src/components/DoctorRequestDetails.js
+++ frontend/src/components/DoctorRequestDetails.js
@@ -1,11 +1,11 @@
 import React, { useState } from 'react';
+// import Doctor from '../../../backend/model/Doctor';
 
 const DoctorRequestDetails = ({ doctorRequest }) => {
     const [isVisible, setIsVisible] = useState(true);
 
-    const handleRejectClick = () => {
-        alert('Rejected');
-        const recordId = doctorRequest._id;
+    const handleClick = () =>{
+      const recordId = doctorRequest._id;
         fetch(`/api/doctorRegistration/doctorRegistrationRequestDelete/${recordId}`, {
             method: 'DELETE', // Use the appropriate HTTP method for your backend API
             headers: {
@@ -26,47 +26,77 @@ const DoctorRequestDetails = ({ doctorRequest }) => {
               console.error('An error occurred:', error);
               // Handle error, if needed
             });
+    };
+
+    const handleRejectClick = () => {
+      const recordId = doctorRequest._id;
+      fetch(`/api/doctorRegistration/doctorRegistrationRequestDelete/${recordId}`, {
+        method: 'DELETE', // Use the appropriate HTTP method for your backend API
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+      .then(function(response) {
+        if (!response.ok) {
+          throw new Error('Record could not be removed');
+        }
+        return response.json();
+      })
+      .then(function(data) {
+              alert('Rejected');
+              console.log('Record has been removed:', data);
+              // Handle success response, if needed
+            })
+            .catch(function(error) {
+              console.error('An error occurred:', error);
+              // Handle error, if needed
+            });
         setIsVisible(false);
     };
 
-    const handleAcceptClick = () => {
+    const handleAcceptClick = async () => {
+      const doctor = {
+        username: doctorRequest.username,
+        name: doctorRequest.name,
+        email: doctorRequest.email,
+        password: doctorRequest.password,
+        dateOfBirth: doctorRequest.dateOfBirth,
+        hourlyRate: doctorRequest.hourlyRate,
+        affiliation: doctorRequest.affiliation,
+        educationalBackground: doctorRequest.educationalBackground,
+        speciality: doctorRequest.speciality,
+        sessionPrice: doctorRequest.sessionPrice
+      };
+      
+      try {
+        const response = await fetch('/api/doctor/createDoctor', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(doctor),
+        });
+        console.log('Response status:', response.status);
+        const data = await response.json();
+        console.log('Response data:', data);
+        
+        if (!response.ok) {
+          throw new Error('Doctor could not be added');
+        }
+        
+        console.log('Doctor has been added:', data);
+        // Handle success response, if needed
+        
         alert('Accepted');
-//         const name = doctorRequest.name;
-//         const userName = doctorRequest.username;
-//         const email = doctorRequest.email;
-//         const dateOfBirth = doctorRequest.dateOfBirth;
-//         const password = doctorRequest.password;
-//         const affiliation = doctorRequest.affiliation;
-//         const educationalBackground = doctorRequest.educationalBackground;
-//         const speciality = doctorRequest.speciality;
-//         const hourlyRate = doctorRequest.hourlyRate;
-
-//   // Perform the addition process here
-//   // Send a request to the backend to add the doctor
-
-//   // Example using fetch:
-//   fetch('/api/doctor/createDoctor', {
-//     method: 'POST', // Use the appropriate HTTP method for your backend API
-//     headers: {
-//       'Content-Type': 'application/json',
-//     },
-//     body: JSON.stringify({ userName, name, email, password, dateOfBirth, hourlyRate, affiliation, educationalBackground, speciality }), // Send the doctor details in the request body
-//   })
-//     .then(function(response) {
-//       if (!response.ok) {
-//         throw new Error('Doctor could not be added');
-//       }
-//       return response.json();
-//     })
-//     .then(function(data) {
-//       console.log('Doctor has been added:', data);
-//       // Handle success response, if needed
-//     })
-//     .catch(function(error) {
-//       console.error('An error occurred:', error);
-//       // Handle error, if needed
-//     });
         setIsVisible(false);
+        handleClick();
+    
+      } catch (error) {
+        console.error('An error occurred:', error);
+        alert('Cant');
+        // Handle error, if needed
+      }
+    
     };
 
     return (
@@ -83,7 +113,7 @@ const DoctorRequestDetails = ({ doctorRequest }) => {
                     <p className="card-text">Speciality: {doctorRequest.speciality}</p>
                     <p className="card-text">Request Status: {doctorRequest.status}</p>
                     <p className="card-text">Created At: {doctorRequest.createdAt}</p>
-                    <button className="btn btn-success" onClick={handleAcceptClick} style={{ marginRight: '5px' }}>Accept</button>
+                    <button className="btn btn-success"  style={{ marginRight: '5px' }} onClick={handleAcceptClick}>Accept</button>
                     <button className="btn btn-danger" onClick={handleRejectClick}>Reject</button>
                 </div>
             </div>
