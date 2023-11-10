5fd13391fb12d8d0439a9c8bf137a181713d1ec5
funny flea <funnyflea7@Yaras-MacBook-Pro.local>
2023-11-10 04:34:41 +0200

    backend &frontend &modification of req sprint1

--- backend/controller/DoctorRegistrationController.js
+++ backend/controller/DoctorRegistrationController.js
@@ -1,7 +1,8 @@
 const DoctorRegistrationModel = require('../model/DoctorRegistration')
 const mongoose = require('mongoose')
 const asyncHandler = require('express-async-handler')
-
+const DoctorModel = require('../model/Doctor')
+const UserModel = require('../model/User')
 //ziad: requirement 3
 //submit a request to register as a doctor using username, name, email, password, date of birth, hourly rate, affiliation, educational background
 const doctorRegistrationRequest = asyncHandler(async (req,res) =>{
@@ -31,7 +32,43 @@ const getDoctorRequests = asyncHandler(async (req, res) => {
 
 
 
+const acceptDoctorRequests = asyncHandler(async (req, res) => {
+    const {id}=req.params
+    try {
+        const Doctor = await DoctorRegistrationModel.findByIdAndRemove(id)
+        const user = await UserModel.create({username: Doctor.username,password: Doctor.password,role:'DOCTOR'})
+        const addDoctor =await DoctorModel.create({username: Doctor.username,name: Doctor.name,email:  Doctor.email,password: Doctor.password,dateOfBirth:  Doctor.dateOfBirth,hourlyRate: Doctor.hourlyRate,affiliation: Doctor.affiliation,educationalBackground: Doctor.educationalBackground,speciality: Doctor.speciality,sessionPrice: Doctor.sessionPrice})
+        res.status(200).json(addDoctor)
+        
+    }
+    catch (error){
+        res.status(400)
+        throw new Error(error.message)
+    }
+    
+    
+})
+
+const rejectDoctorRequests = asyncHandler(async (req, res) => {
+    const {id}=req.params
+    try {
+        const Doctor = await DoctorRegistrationModel.findByIdAndRemove(id)
+        res.status(200).json(Doctor)
+        
+    }
+    catch (error){
+        res.status(400)
+        throw new Error(error.message)
+    }
+    
+    
+})
+
+
+
 module.exports = {
     doctorRegistrationRequest,
     getDoctorRequests,
+    acceptDoctorRequests,
+    rejectDoctorRequests
 };
--- frontend/src/components/DoctorRequestDetails.js
+++ frontend/src/components/DoctorRequestDetails.js
@@ -1,6 +1,43 @@
 import React from 'react';
 
 const DoctorRequestDetails = ({ doctorRequest }) => {
+    const handleAcceptDoctor = async (e) => {
+        e.preventDefault()
+        //const doctorRequest = {username: username, name: name, email: email, password: password, dateOfBirth: dateOfBirth, hourlyRate: hourlyRate, affiliation: affiliation, educationalBackground: educationalBackground,speciality:speciality,sessionPrice:sessionPrice}
+        const id=doctorRequest._id
+        const response = await fetch('/api/doctorRegistration/acceptDoctorRequests/'+id, {
+          method: 'POST',
+          body: JSON.stringify(),
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        })
+        if (!response.ok) {
+            alert(await response.text())
+        }
+        if (response.ok) {
+            alert("doctor is accepted and added successfuly");
+    
+        }
+      }
+    
+      const handleRejectDoctor = async (e) => {
+        e.preventDefault()
+        const id=doctorRequest._id
+        const response = await fetch('/api/doctorRegistration/rejectDoctorRequests/'+id, {
+          method: 'DELETE',
+          body: JSON.stringify(),
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        })
+        if (!response.ok) {
+            alert(await response.text())
+        }
+        if (response.ok) {
+            alert("doctor is rejected");
+        }
+      }
     return (
         <div className="card">
             <div className="card-body">
@@ -14,8 +51,8 @@ const DoctorRequestDetails = ({ doctorRequest }) => {
                 <p className="card-text">Speciality: {doctorRequest.speciality}</p>
                 <p className="card-text">Request Status: {doctorRequest.status}</p>
                 <p className="card-text">Created At: {doctorRequest.createdAt}</p>
-                <button className="btn btn-success">Accept</button>
-                <button className="btn btn-danger">Reject</button>
+                <button className="btn btn-success" onClick={handleAcceptDoctor}>Accept</button>
+                <button className="btn btn-danger" onClick={handleRejectDoctor}>Reject</button>
             </div>
         </div>
     );
