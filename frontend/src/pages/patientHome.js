346b7d59c2e0faa4905fb59e0fd841afafc481a6
WoaodMoubarak <127627787+WoaodMoubarak@users.noreply.github.com>
2023-12-09 18:13:36 +0200

    patientHomepage

--- frontend/src/pages/patientHome.js
+++ frontend/src/pages/patientHome.js
@@ -1,6 +1,6 @@
 import { useNavigate } from 'react-router-dom';
 import React from "react";
-
+import hrtImage from '../images/hrt.jpeg'; // Import the image
 
 const PatientHome = () => {
 
@@ -11,20 +11,20 @@ const PatientHome = () => {
     };
     return(
 
-        <body className="my-patient-background">
-      
+       
+        <div className="container">
         <div className="row">
-        <div class="welcome-page">
-            <div className="col-4">
+      
+            <div className="col-6">
             
                 <h2 className="margin-edit">
                 <h2 className="welcomeHeader">Welcome To Our Clinic </h2>
                                 
-                    
                     <p> Experience seamless care </p>
                     <p>  at our Virtual Clinic </p>
-                    <p> consult with experts from anywhere,</p>
-                    <p> anytime, with just a click</p>
+                    <p> consult with experts from </p>
+                    <p> anywhere,anytime, </p>
+                    <p>  with just a click</p>
                     <button className="patient-btn" onClick={handleBack}>
                         Reserve an Appointment
                     </button>
@@ -32,11 +32,15 @@ const PatientHome = () => {
                 </h2>
 
             </div>
-          
+           
+            <div className="col-6">
+            <h2 className="margin-edit">
+           <img className=''src={require('../images/hrt.jpeg')}/>
+           </h2>
             </div>
             </div>
-
-        </body>
+            </div>
+      
     )
 }
 export default PatientHome;
